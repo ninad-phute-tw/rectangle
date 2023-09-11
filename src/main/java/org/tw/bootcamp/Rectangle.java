1e22fb55799c8c67b3e13210e8d94ffd5312ea49
Sankaran Srinivasan <sankaran.srinivasan@Sankarans-MacBook-Pro-2.local>
2023-09-11 15:14:48 +0530

    Add logic to compute perimeter of the rectangle

--- src/main/java/org/tw/bootcamp/Rectangle.java
+++ src/main/java/org/tw/bootcamp/Rectangle.java
@@ -5,12 +5,16 @@ public class Rectangle {
     private final int length;
     private final int breadth;
 
-    public Rectangle(int len, int breadth) {
-        this.length = len;
+    public Rectangle(int length, int breadth) {
+        this.length = length;
         this.breadth = breadth;
     }
 
-    public double calculateArea() {
+    public double area() {
         return length * breadth;
+    }
+
+    public double perimeter() {
+        return 2 * (length + breadth);
     }
 }
---
+++ src/test/java/RectangleTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+import org.tw.bootcamp.Rectangle;
+
+public class RectangleTest {
+
+    @Test
+    public void shouldReturnAreaForValidLengthAndBreadth() {
+        final int length = 10;
+        final int breadth = 10;
+
+        final Rectangle rectangle = new Rectangle(length, breadth);
+        double area = rectangle.area();
+
+        assertEquals(100, area);
+    }
+
+    @Test
+    void shouldReturnPerimeterForValidLengthAndBreadth(){
+        int length = 10;
+        int breadth = 5;
+
+        Rectangle rectangle = new Rectangle(length, breadth);
+        double perimeter = rectangle.perimeter();
+
+        assertEquals(30, perimeter);
+    }
+}
